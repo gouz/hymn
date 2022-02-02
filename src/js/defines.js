c3ea218e96b1030040b60e8ca47860abd854de03
Sylvain Gougouzian <sylvain@lidrea.fr>
2022-02-02 01:50:56 +0100

    feat: :sparkles: cypher the music

--- src/js/defines.js
+++ src/js/defines.js
@@ -1,17 +1,4 @@
-import Vex from "vexflow";
-
-const vf = new Vex.Flow.Factory({
-  renderer: {
-    elementId: "score",
-    width: 500,
-    height: 300,
-  },
-});
-
 window.$hymn = {
   $in: document.getElementById("in"),
   $transform: document.getElementById("transform"),
-  score: vf.EasyScore(),
-  system: vf.System(),
-  vf: vf,
 };
--- src/js/methods/transform.js
+++ src/js/methods/transform.js
@@ -1,16 +1,82 @@
 window.$hymn.transform = () => {
   // fetch and clean string
-  const userString = window.$hymn.$in.value
+  let userString = window.$hymn.$in.value
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-z]/gim, " ")
+    .replace(/\s+/g, "")
     .toLowerCase();
+  // french replacement
+  const remplacements = {
+    // french
+    do: "C",
+    dau: "C",
+    re: "D",
+    rai: "D",
+    mi: "E",
+    my: "E",
+    me: "E",
+    fa: "F",
+    pha: "F",
+    sol: "G",
+    saul: "G",
+    la: "A",
+    lo: "A",
+    l: "A",
+    si: "B",
+    sy: "B",
+    ut: "C",
+    u: "C",
+    r: "D",
+    // german
+    es: "E-",
+    as: "A-",
+    gis: "G#",
+    a: "A",
+    b: "B",
+    c: "C",
+    d: "D",
+    e: "E",
+    i: "E",
+    f: "F",
+    g: "G",
+    h: "B-",
+    s: "E-",
+    z: "E-",
+    // minor become flat
+    Am: "A-",
+    Bm: "B-",
+    Cm: "C-",
+    Dm: "D-",
+    Em: "E-",
+    Fm: "F-",
+    Gm: "G-",
+    // dur
+    Ax: "A#",
+    Bx: "B#",
+    Cx: "C#",
+    Dx: "D#",
+    Ex: "E#",
+    Fx: "F#",
+    Gx: "G#",
+    // others
+    q: "C",
+    m: "E",
+    n: "E",
+    k: "C",
+    v: "B",
+    y: "E",
+    p: "B",
+    t: "B",
+    o: "A",
+    w: "B",
+  };
+  Object.keys(remplacements).forEach((n) => {
+    userString = userString.replace(new RegExp(n, "g"), remplacements[n]);
+  });
   let notes = [];
   userString.split("").forEach((c) => {
-    switch (c) {
-      case "a":
-        notes.push("(A2 C3 E3)/q");
-        break;
-    }
+    notes.push(c);
   });
-  window.$hymn.render(["D5/q/r", notes.join(",")]);
+  console.log(notes);
 };
